e617d3d842ff484ebd2f1fbdb3d71f94da6062b8
GAGAN-Rokiee <gagan.gdarya@gmail.com>
2024-06-25 18:33:07 +0530

    Updated Package.json

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/job-portal/" ,
     plugins: [react()],
     server: {
         host: true, // Here
